5fdc0928820148a897ac930d671ff7c0232e9f04
alex-e-adams <92957824+alex-e-adams@users.noreply.github.com>
2022-11-10 13:47:59 -0500

    it works, need to fix warnings

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::env;
-use std::net::IpAddr;
+use std::io::{self, Write};
+use std::net::{IpAddr, TcpStream};
 use std::str::FromStr;
 use std::process;
+use std::sync::mpsc::{Sender, channel};
+use std::thread;
+
+const MAX: u16 = 65535;
 
 struct Arguments {
     flag: String,
@@ -23,7 +28,7 @@ impl Arguments {
             let flag = args[1].clone();
             if flag.contains("-h") || flag.contains("-help") && args.len() == 2 {
                 println!("Usage: -j to select how many threads you want
-                \r\n -h or -help to show this help message");
+                \r\n       -h or -help to show this help message");
                 return Err("help");
             } else if flag.contains("-h") || flag.contains("-help") {
                 return Err("too many arguments");
@@ -44,6 +49,24 @@ impl Arguments {
     }
 }
 
+fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
+    let mut port: u16 = start_port + 1;
+    loop {
+        match TcpStream::connect((addr, port)) {
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) => {}
+        }
+
+        if (MAX - port) <= num_threads {
+            break;
+        }
+        port += num_threads;
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -57,5 +80,27 @@ fn main() {
                 process::exit(0);
             }
         }
-    )
+    );
+
+    let num_threads = arguments.threads;
+    let(tx, rx) = channel();
+    for i in 0..num_threads {
+        let tx = tx.clone();
+
+        thread:: spawn(move || {
+            scan(tx, i, arguments.ipaddr, num_threads);
+        });
+    }
+
+    let mut out = vec![];
+    drop(tx);
+    for p in rx {
+        out.push(p);
+    }
+
+    println!("");
+    out.sort();
+    for v in out {
+        println!("{} is open", v);
+    }
 }
